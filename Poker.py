711322d7677edd05679eab12d70cf02c0edc76da
UltraArcher <ultra.archerhm@gmail.com>
2024-07-18 10:40:42 +1000

    fixed the print_card() function and added stuff for the flop

--- Poker.py
+++ Poker.py
@@ -5,6 +5,7 @@ class Card:
         self.name = name
         self.suit = suit
         self.value = value
+        self.id = id
  
     def print_card(self):
         lines = []
@@ -31,7 +32,7 @@ class Deck:
         for i in range(len(values)):
             for suit in suits:
                 self.cards.append(Card(names[i], suit, values[i]))
-                self.cardsid.append(names + ids)
+                self.cardsid.append(names[i] + ids[i % len(ids)])
  
         self.shuffle()
  
@@ -43,6 +44,11 @@ class Deck:
         print(card)
         return card
 
+    def print_deck(self):
+        for card in self.cards:
+            for line in card.print_card():
+                print(line)
+
 class Player:
     def __init__(self):
         self.hand = []
@@ -52,21 +58,44 @@ class Player:
         for _ in range(2):
             card = deck.draw()
             self.hand.append(card)
-            self.hand_id.append(card.name + card.suit)
+            self.hand_id.append(card.name + card.ids)
 
     def print_hand(self):
-        card_lines = []
+        card_lines = [[] for _ in range(5)]
         for card in self.hand:
-            card_lines.append(card.print_card())
+            lines = card.print_card()
+            for i, line in enumerate(lines):
+                card_lines[i].extend(line)
 
-        for i in range(len(card_lines[0])):
-            line = ""
-            for j in range(len(card_lines)):
-                line += card_lines[j][i] + "  "
-            print(line)
+        for line in card_lines:
+            print("  ".join(line))
+        print()
+
+class Flop:
+    def __init__(self, deck):
+        self.cards = []
+        for _ in range(3):
+            card = deck.draw()
+            self.cards.append(card)
+
+    def print_flop(self):
+        flop_lines = [[] for _ in range(5)]
+        for card in self.cards:
+            lines = card.print_card()
+            for i, line in enumerate(lines):
+                flop_lines[i].extend(line)
+
+        for line in flop_lines:
+            print("  ".join(line))
+        print()
 
 deck = Deck()
 player = Player()
 player.draw_hand(deck)
 player.print_hand()
-print(Player.hand_id)
+print(player.hand_id)
+
+flop = Flop(deck)
+flop.print_flop()
+
+deck.print_deck()
